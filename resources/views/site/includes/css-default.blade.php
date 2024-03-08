da6a66f73279e0407e6e3a9d12d73779f81e744f
Felipe Palmeira <felipedpaula97@gmail.com>
2024-03-08 16:23:30 -0300

    Avanços header

--- resources/views/site/includes/css-default.blade.php
+++ resources/views/site/includes/css-default.blade.php
@@ -5,3 +5,4 @@
 <!-- Style CSS -->
 <link rel="stylesheet" href="{{asset('assets/site/css/style.css')}}">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
