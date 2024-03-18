7a5ff6c66da465a3a392ca1520787a338f82f546
Felipe Palmeira <felipedpaula97@gmail.com>
2024-03-18 17:26:44 -0300

    Header finalizado | Slider Home

--- resources/views/site/includes/scripts-default.blade.php
+++ resources/views/site/includes/scripts-default.blade.php
@@ -6,7 +6,7 @@
 
 
 <!--Plugins JS-->
-
+<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
 
 <!--Main JS (Common Activation Codes)-->
 <script src="{{asset('assets/site/scripts/main.js')}}"></script>
--- resources/views/site/pages/home/index.blade.php
+++ resources/views/site/pages/home/index.blade.php
@@ -1,8 +1,47 @@
 @extends('site.layouts.site-default')
 
 @section('content')
-    {{-- <p>$destaques_slide</p>
-    <p>$destaques_pequenos</p>
-    <p>$ofertas_especiais</p>
-    <p>$categorias</p> --}}
+    <section class="main-section section-1">
+        {{-- <p>$destaques_slide</p> --}}
+        <div class="swiper swiper-1">
+            <!-- Additional required wrapper -->
+            <div class="swiper-wrapper">
+                <!-- Slides -->
+                <div class="swiper-slide">
+                    <div class="container">
+                        <div class="main-slide">
+                            Slide teste
+                        </div>
+                    </div>
+                </div>
+                <div class="swiper-slide">
+                    <div class="container">
+                        <div class="main-slide">
+                            Slide teste
+                        </div>
+                    </div>
+                </div>
+                <div class="swiper-slide">
+                    <div class="container">
+                        <div class="main-slide">
+                            Slide teste
+                        </div>
+                    </div>
+                </div>
+              ...
+            </div>
+            <!-- If we need pagination -->
+            <div class="swiper-pagination"></div>
+
+            <!-- If we need navigation buttons -->
+            <div class="swiper-button-prev"></div>
+            <div class="swiper-button-next"></div>
+
+            <!-- If we need scrollbar -->
+            <div class="swiper-scrollbar"></div>
+        </div>
+    </section>
+    {{-- <p>$destaques_pequenos</p> --}}
+    {{-- <p>$ofertas_especiais</p> --}}
+    {{-- <p>$categorias</p> --}}
 @endsection
